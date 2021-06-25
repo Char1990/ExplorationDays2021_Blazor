ae7542fd99141516a5950e47e696d5a465383351
Ilyasse Lahraoui <ilyasse.lahraoui@cegeka.com>
2021-06-25 15:56:59 +0200

    Added workflow context

---
+++ WorkflowVisualisation/WorkflowVisualisation.WebAssemblyNetCoreHosted/Server/Controllers/WorkflowController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkflowVisualisation.WebAssemblyNetCoreHosted.Server.DBContext;
+using WorkflowVisualisation.WebAssemblyNetCoreHosted.Shared;
+
+namespace WorkflowVisualisation.WebAssemblyNetCoreHosted.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WorkflowController : ControllerBase
+    {
+
+        private readonly ILogger<WorkflowController> _logger;
+        private WorkflowContext _context;
+
+        public WorkflowController(ILogger<WorkflowController> logger, WorkflowContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Workflow> Get()
+        {
+            return _context.Workflows;
+        }
+    }
+}
---
+++ WorkflowVisualisation/WorkflowVisualisation.WebAssemblyNetCoreHosted/Server/DBContext/WorkflowContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkflowVisualisation.WebAssemblyNetCoreHosted.Shared;
+
+namespace WorkflowVisualisation.WebAssemblyNetCoreHosted.Server.DBContext
+{
+    public class WorkflowContext : DbContext
+    {
+        public WorkflowContext()
+        {
+        }
+
+        public WorkflowContext(DbContextOptions<WorkflowContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Workflow> Workflows { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Workflow>(entity =>
+            {
+                entity.ToTable("Book");
+            });
+
+        }
+    }
+}
